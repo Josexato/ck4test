19bb0960771bf909d082f54d6cd584b83803970f
Josexato <josexato@gmail.com>
2020-06-21 13:11:08 -0500

    actualización la generación de etiquetas

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -32,6 +32,39 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
+        // Get a list of tags.
+        $tags = $this->Articles->Tags->find('list');
+
+        // Set tags to the view context
+        $this->set('tags', $tags);
+
         $this->set('article', $article);
+    }
+    
+    public function edit($slug)
+    {
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->firstOrFail();
+        if ($this->request->is(['post', 'put']))
+        {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) 
+            {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+        $this->set('article', $article);    
+    }
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->Articles->delete($article)) {
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
     }
 }
--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -1,13 +1,15 @@
 <h1>Articles</h1>
+<p><?= $this->Html->link("Add Article", ['action' => 'add']) ?></p>
 <table>
     <tr>
         <th>Title</th>
         <th>Created</th>
+        <th>Action</th>
     </tr>
 
-    <!-- Here is where we iterate through our $articles query object, printing out article info -->
+<!-- Here's where we iterate through our $articles query object, printing out article info -->
 
-    <?php foreach ($articles as $article): ?>
+<?php foreach ($articles as $article): ?>
     <tr>
         <td>
             <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
@@ -15,6 +17,15 @@
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
         </td>
+        <td>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+            <?= $this->Form->postLink(
+                'Delete',
+                ['action' => 'delete', $article->slug],
+                ['confirm' => 'Are you sure?'])
+            ?>
+        </td>
     </tr>
-    <?php endforeach; ?>
+<?php endforeach; ?>
+
 </table>
